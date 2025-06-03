66024ba7aba3dfd41d098257cb94eb703dc558ce
neuronumcybernetics <welcome@neuronum.net>
2025-06-03 22:39:55 +0200

    examples update

--- examples/transmitters/list_transmitters.py
+++ examples/transmitters/list_transmitters.py
@@ -1,14 +1,12 @@
 import asyncio
 import neuronum
 
-
 cell = neuronum.Cell(                                                       # set Cell connection
     host="host",                                                            # Cell host
     password="password",                                                    # Cell password
     network="neuronum.net",                                                 # Cell network -> neuronum.net
     synapse="synapse"                                                       # Cell synapse
 )
-
 
 async def main():
                                                             
--- examples/transmitters/public_transmitter.py
+++ examples/transmitters/public_transmitter.py
@@ -1,7 +1,6 @@
 import asyncio
 import neuronum
 
-
 cell = neuronum.Cell(                                                       # set Cell connection
     host="host",                                                            # Cell host
     password="password",                                                    # Cell password
@@ -9,35 +8,27 @@ cell = neuronum.Cell(                                                       # se
     synapse="synapse"                                                       # Cell synapse
 )
 
-
 async def main():
 
     descr = "Test Transmitter"                                              # describe your Transmitter TX (max 25 characters)
-
     key_values = {                                                          # define keys and example values
         "key1": "valueX",
         "key2": "valueY",
     }
-
-    STX = "CMTY_G2AjiEAgIF::stx"                                            # select the Stream (STX) you want the data to be sent to
-                                       
+    STX = "CMTY_G2AjiEAgIF::stx"                                            # select the Stream (STX) you want the data to be sent to                            
     label = "labelroot:key1:key2"                                           # label the Transmitter TX data
                                                                             # Labels are dynamically created for each activated Transmitter TX
                                                                             # This Transmitter TX will have the Label: labelroot:valueX:valueY
-
     partners = ["public"]                                                   # a public Transmitter TX all Cells can execute
-
     txID = await cell.create_tx(descr, key_values, STX, label, partners)    # create the Transmitter TX -> get txID back
-    
     print(f"Transmitter ID: {txID}")                                        # print the Transmitter TX ID
 
-    TX = txID                                                               # select the Transmitter TX
 
+    TX = txID                                                               # select the Transmitter TX
     data = {                                                                # enter value data you want to transmit with predefined keys 
         "key1": "valueX",
         "key2": "valueY",
     }
-
     await cell.activate_tx(TX, data)                                        # activate TX - > get success message back
 
 asyncio.run(main())
